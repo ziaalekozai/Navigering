22000dc867e7cf66e6a220c624537844ac3a4e63
ziaalekozai <88ZiaAle@gafe.molndal.se>
2020-10-02 12:55:15 +0200

    new changes

--- Uppgift_2/Program.cs
+++ Uppgift_2/Program.cs
@@ -8,41 +8,37 @@ namespace Uppgift_2
     {
         static void Main(string[] args)
         {
-            
+            DirectoryInfo start_om = new DirectoryInfo(@"C:\Users\88ziaale\Desktop\C# Data");
             DirectoryInfo skrivBord = new DirectoryInfo(@"C:\Users\88ziaale\Desktop\C# Data");
            start: 
                foreach (var dir in skrivBord.GetDirectories())
-               {
-
-                   foreach (var file in skrivBord.GetFileSystemInfos())
+               { 
+                foreach (var file in skrivBord.GetFileSystemInfos())
                    {
                        Console.WriteLine(file);
                    }
 
 
-                   if (Console.ReadKey().Key == ConsoleKey.UpArrow)
-                   {
-                       if (skrivBord.ToString() != @"C:\")
-                       {
-                            skrivBord = skrivBord.Parent;
-                            goto start;
-                       }
-                    
-                   }
-
-                   else if (Console.ReadKey().Key == ConsoleKey.DownArrow)
-                   {
-                       skrivBord = skrivBord.Root;
-                       goto start;
-                   }
-
-                   else
-                   {
+                if(Console.ReadKey().Key == ConsoleKey.DownArrow)
+                { 
+                    if (skrivBord.ToString() != @"C:\")
+                    {
+                        skrivBord = skrivBord.Parent;
+                        goto start;
+                    }
+                }
+                else if(Console.ReadKey().Key == ConsoleKey.UpArrow)
+                {
+                       skrivBord = start_om;
+                }
+                else
+                {
                        Console.WriteLine();
                        Console.WriteLine("Du kan bara använda pilen uppåt och pilen neråt");
                        Console.WriteLine();
                         goto start;
-                   }
+
+                }
                }
         }
     }
