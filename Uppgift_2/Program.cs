fc32610be8f7289e2880dbe4ec4e1e8deafc3519
ziaalekozai <88ZiaAle@gafe.molndal.se>
2020-10-02 11:23:42 +0200

    new code changes

--- Uppgift_2/Program.cs
+++ Uppgift_2/Program.cs
@@ -11,43 +11,39 @@ namespace Uppgift_2
             
             DirectoryInfo skrivBord = new DirectoryInfo(@"C:\Users\88ziaale\Desktop\C# Data");
            start: 
-           if (parent == null)
-            {
+               foreach (var dir in skrivBord.GetDirectories())
+               {
 
-            }
+                   foreach (var file in skrivBord.GetFileSystemInfos())
+                   {
+                       Console.WriteLine(file);
+                   }
 
-            
-            foreach (var dir in skrivBord.GetDirectories())
-            {
-                
-                foreach (var file in skrivBord.GetFileSystemInfos())
-                {
-                    Console.WriteLine(file);
-                }
-                
 
-                if (Console.ReadKey().Key == ConsoleKey.UpArrow)
-                {
-                    skrivBord = skrivBord.Parent;
-                    if (skrivBord = !)
-                    {
-                        Console.WriteLine();
-                    }
-                }
+                   if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+                   {
+                       if (skrivBord.ToString() != @"C:\")
+                       {
+                            skrivBord = skrivBord.Parent;
+                            goto start;
+                       }
+                    
+                   }
 
-                else if (Console.ReadKey().Key == ConsoleKey.DownArrow)
-                {
-                    skrivBord = skrivBord.Root;
-                    goto start;
-                }
+                   else if (Console.ReadKey().Key == ConsoleKey.DownArrow)
+                   {
+                       skrivBord = skrivBord.Root;
+                       goto start;
+                   }
 
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Du kan bara använda pilen uppåt och pilen neråt");
-                }
-                goto start;
-            }
+                   else
+                   {
+                       Console.WriteLine();
+                       Console.WriteLine("Du kan bara använda pilen uppåt och pilen neråt");
+                       Console.WriteLine();
+                        goto start;
+                   }
+               }
         }
     }
 }
