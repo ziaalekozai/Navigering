19644c22798cf0bef760b4bccdca80db646b2051
Zia Alekozai <88ziaale@gafe.molndal.se>
2020-11-04 21:37:17 +0100

    använd while

---
+++ Uppgift_2/Navigerare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Uppgift_2
+{
+    class Navigerare
+
+    //private DirectoryInfo aktuell_folder;
+    //TODO konstruktor där man skickar in en 'path' vart den ska börja
+    {
+        string path = Directory.GetCurrentDirectory();
+        DirectoryInfo dir = new DirectoryInfo(@"c:\");
+    }
+    
+    
+
+        //TODO en metod GåUppEnFolder()
+   
+        //TODO en metod VäljNästaFolder()
+        //TODO en metod VäljFöregåendeFolder()
+
+        //TODO en metod GåNerValdFolder()
+
+        //TODO en metod VisaNuvarandeFolderInnehåll()
+    
+}
--- Uppgift_2/Program.cs
+++ Uppgift_2/Program.cs
@@ -1,42 +1,142 @@
 ﻿using System;
 using System.ComponentModel.Design;
 using System.IO;
+using System.Linq;
 
 namespace Uppgift_2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DirectoryInfo start_om = new DirectoryInfo(@"C:\Users\88ziaale\Desktop\C# Data");
-            DirectoryInfo skrivBord = new DirectoryInfo(@"C:\Users\88ziaale\Desktop\C# Data");
-           start: 
-                foreach (var dir in skrivBord.GetDirectories()) 
-            { 
-                foreach (var file in skrivBord.GetFileSystemInfos())
-                   {
-                       Console.WriteLine(file);
-                   }
-                
-                if(Console.ReadKey().Key == ConsoleKey.DownArrow)
-                { 
-                    if (skrivBord.ToString() != @"C:\")
-                    {
-                        skrivBord = skrivBord.Parent;
-                        goto start;
-                    }
-                }
-                else if(Console.ReadKey().Key == ConsoleKey.UpArrow)
+    //OBS Skriv rapporten samtidigt som du kodar!
+    /*
+    Här kan du skriva rapporten
+    */
+
+    /* Vad mitt programm ska göra!
+
+    1 kunna visa alla filer i en folder
+
+    2 kunna gå till föräldrafoldern (om användaren trycker på pil upp) och visa filer där
+
+    3 kunna välja en barn folder visuellt med höger/vänster pil
+        help.txt         2014-04-12           4kb
+        my_files         2014-05-11           --
+        folder           2014-05-11           --             <--
+        another_folder   2014-05-11           --
+
+        (trycker på höger pil) 
+
+        help.txt         2014-04-12           4kb
+        my_files         2014-05-11           --
+        folder           2014-05-11           --             
+        another_folder   2014-05-11           --             <--
+
+    4 kunna gå ner till den valda foldern med 'pil ner'
+
+    5 Skapa Navigerare klassen som håller koll på DirectoryInfo objektet
+
+    */
+   class Program
+   {
+       static void Main(string[] args)
+       {
+            Console.WriteLine("Hej och vällkomen! \n\nFör att vissa filer trycka på pilen upp och för att vissa folder tryck på pilen ner");
+            Console.WriteLine();
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\88ziaale\source\repos\Testing\Testing");
+         
+           foreach (var folder in dir.GetDirectories())
+            {
+               foreach (var file in dir.GetFiles())
                 {
-                       skrivBord = start_om;
-                }
-                else
-                {   Console.WriteLine();
-                    Console.WriteLine("Du kan bara använda pilen uppåt och pilen neråt");
-                    Console.WriteLine();
-                    goto start;
-                }
-            }
-        }
-    }
+                    while (true)
+                    {
+                     try
+                     {
+                       if(Console.ReadKey().Key == ConsoleKey.DownArrow)
+                       {
+                         Console.WriteLine("Folder: {0} \t\t Skapad: {1}", folder.FullName, folder.CreationTime);
+                       }
+                       else if(Console.ReadKey().Key == ConsoleKey.UpArrow)
+                       {
+                       Console.WriteLine("File: {0} \t Skapad: {1}\tStorlek: {2}kb",file.FullName, file.CreationTime, file.Length);
+                       }
+                     }
+                        catch (Exception)
+                            {
+                                throw;
+                            }
+
+                   }
+               }
+           }
+       }
+   }
 }
+           
+        //} while (true)
+        //    {
+
+        //        if (Console.ReadKey().Key == ConsoleKey.DownArrow)
+        //        {
+        //            if (dir.ToString() != @"C:\")
+        //            {
+        //                dir = dir.Parent;
+        //                //TODO pröva att använda 'continue'
+        //            }
+        //        }
+        //        else if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+        //        {
+
+        //        }
+        //        else
+        //        {
+        //            //TODO Gör så att man kan välja vilken folder man går ner till via höger/vänster pil
+
+        //        }
+        //    }
+  
+      //{
+            //    //Kolla vad användaren gör
+            //    if (Console.ReadKey().Key == ConsoleKey.DownArrow)
+            //    {
+            //        //gör något beroende på användaren
+            //        if (skrivBord.ToString() != @"C:\")
+            //        {
+            //            skrivBord = skrivBord.Parent;
+            //        }
+            //    }
+
+
+            //    //visa resultatet
+            //    foreach (var file in skrivBord.GetFileSystemInfos())
+            //    {
+            //        Console.WriteLine(file);
+            //        skrivBord = skrivBord.Parent;
+            //    }
+            //}
+        
+        //{
+        //    string path = Directory.GetCurrentDirectory();
+        //    DirectoryInfo skrivbord = new DirectoryInfo(path); //TODO använd path variablen istället för denna 'absolute path' du har nu
+        //    Console.WriteLine(skrivbord);
+
+
+
+
+        ///*
+        //start: //TODO Använd en while-sats istället för goto
+
+        //    foreach (var dir in skrivBord.GetDirectories())
+        //    {
+        //        foreach (var file in skrivBord.GetFileSystemInfos())
+        //        {
+        //            Console.WriteLine(file);
+        //        }
+
+        //            goto start;
+
+        //        }
+        //    }
+        //*/
+        //
+
+ 
+            
